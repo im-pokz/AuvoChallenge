459f4029e14d45cbbebbe30974929facc3ecdfae
Pietro Niero Roque <pietronieroboss@gmail.com>
2021-06-27 20:18:56 -0300

    App locale e front de edicao

--- AuvoChallenge/Models/Transfer.cs
+++ AuvoChallenge/Models/Transfer.cs
@@ -8,12 +8,21 @@ namespace AuvoChallenge.Models
 {
     public class Transfer
     {
+        [Display(Name = "Código")]
         public int Id { get; set; }
+
+        [Display(Name = "Nome")]
         public string Name { get; set; }
 
+        [Display(Name = "Valor")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
         public double Value { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Data")]
         public DateTime Date { get; set; }
+
+        [Display(Name = "Observação")]
         public string Observation { get; set; }
 
         public Transfer()
